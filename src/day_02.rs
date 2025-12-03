837e1b94eb504900785b6aa33aff7c1970886efc
Sam Rowe <sgrowe@live.co.uk>
2025-12-03 10:01:19 +0000

    Solve day 2 part 2

--- src/day_02.rs
+++ src/day_02.rs
@@ -23,28 +23,35 @@ impl Aoc for DayTwo {
     }
 
     fn part_one(&mut self) -> isize {
-        self.invalid_ids().sum::<usize>() as isize
+        self.invalid_ids_pt1().sum::<usize>() as isize
     }
 
     fn part_two(&mut self) -> isize {
-        0
+        self.invalid_ids_pt2().sum::<usize>() as isize
     }
 }
 
 impl DayTwo {
-    fn invalid_ids(&self) -> impl Iterator<Item = usize> {
+    fn invalid_ids_pt1(&self) -> impl Iterator<Item = usize> {
         self.ranges
             .iter()
             .cloned()
             .flat_map(|r| r)
-            .filter(|&x| is_invalid(x))
+            .filter(|&x| is_invalid_pt1(x))
+    }
+
+    fn invalid_ids_pt2(&self) -> impl Iterator<Item = usize> {
+        self.ranges
+            .iter()
+            .cloned()
+            .flat_map(|r| r)
+            .filter(|&x| is_invalid_pt2(x))
     }
 }
 
-fn is_invalid(id: usize) -> bool {
+fn is_invalid_pt1(id: usize) -> bool {
     let as_str = id.to_string();
 
-    // TODO:
     if as_str.len() % 2 != 0 {
         return false;
     }
@@ -52,6 +59,29 @@ fn is_invalid(id: usize) -> bool {
     let mid = as_str.len() / 2;
 
     return &as_str[0..mid] == &as_str[mid..];
+}
+
+fn is_invalid_pt2(id: usize) -> bool {
+    let as_str = id.to_string();
+
+    for seq_len in 1..=as_str.len() / 2 {
+        if as_str.len() % seq_len != 0 {
+            continue;
+        }
+
+        let seq = &as_str[0..seq_len];
+
+        let repeats = as_str.len() / seq_len;
+
+        if (1..repeats)
+            .map(|j| &as_str[(j * seq_len)..((j + 1) * seq_len)])
+            .all(|next_seq| seq == next_seq)
+        {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 #[cfg(test)]
@@ -62,11 +92,24 @@ mod tests {
 
     #[test]
     fn part_one_example() {
-        let result: Vec<_> = DayTwo::new(EXAMPLE.to_string()).invalid_ids().collect();
+        let result: Vec<_> = DayTwo::new(EXAMPLE.to_string()).invalid_ids_pt1().collect();
 
         assert_eq!(
             result,
             vec![11, 22, 99, 1010, 1188511885, 222222, 446446, 38593859]
+        )
+    }
+
+    #[test]
+    fn part_two_example() {
+        let result: Vec<_> = DayTwo::new(EXAMPLE.to_string()).invalid_ids_pt2().collect();
+
+        assert_eq!(
+            result,
+            vec![
+                11, 22, 99, 111, 999, 1010, 1188511885, 222222, 446446, 38593859, 565656,
+                824824824, 2121212121
+            ]
         )
     }
 }
