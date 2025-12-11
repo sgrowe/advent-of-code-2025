508bdc5bf75f0e3b20e1bac8ee296cdc9a17f5fc
Sam Rowe <sgrowe@live.co.uk>
2025-12-11 16:32:25 +0000

    Optimise day 4

--- src/day_04.rs
+++ src/day_04.rs
@@ -1,31 +1,45 @@
+use std::collections::HashSet;
+
 use crate::aoc::Aoc;
 
 #[derive(Debug)]
 pub struct DayFour {
     row_len: usize,
-    grid: Vec<bool>,
+    col_len: usize,
+    spots_with_rolls: HashSet<usize>,
 }
+
+type Coord = (usize, usize);
 
 impl Aoc for DayFour {
     type PtOne = usize;
     type PtTwo = usize;
 
     fn new(input: String) -> Self {
-        let mut num_rows = 0;
+        let mut total = 0;
+        let mut col_len = 0;
 
-        let grid: Vec<bool> = input
+        let spots: HashSet<_> = input
             .trim()
             .lines()
+            .flat_map(|l| {
+                col_len += 1;
+                l.chars()
+            })
             .enumerate()
-            .flat_map(|(i, l)| {
-                num_rows = i + 1;
-                l.chars().map(|c| c == '@')
+            .filter_map(|(i, c)| {
+                total = i + 1;
+
+                if c == '@' { Some(i) } else { None }
             })
             .collect();
 
+        let row_len = total / col_len;
+
         DayFour {
-            row_len: grid.len() / num_rows,
-            grid,
+            row_len,
+            col_len,
+            spots_with_rolls: spots,
         }
     }
 
@@ -38,8 +52,8 @@ impl Aoc for DayFour {
         let mut count = spots.len();
 
         while spots.len() > 0 {
-            for c in spots {
-                *self.get_mut(c) = false;
+            for x in spots {
+                self.spots_with_rolls.remove(&x);
             }
 
             spots = self.accessible_spots().collect();
@@ -50,15 +64,20 @@ impl Aoc for DayFour {
     }
 }
 
-type Coord = (usize, usize);
-
 impl DayFour {
-    fn accessible_spots(&self) -> impl Iterator<Item = Coord> {
-        self.all_coords()
-            .filter(|&c| self.get(c) && self.adjacent_to(c).filter(|&c| self.get(c)).count() < 4)
+    fn accessible_spots(&self) -> impl Iterator<Item = usize> {
+        self.spots_with_rolls.iter().copied().filter(|&c| {
+            self.adjacent_to(c)
+                .filter(|c| self.spots_with_rolls.contains(c))
+                .count()
+                < 4
+        })
     }
 
-    fn adjacent_to(&self, (x, y): Coord) -> impl Iterator<Item = Coord> {
+    fn adjacent_to(&self, i: usize) -> impl Iterator<Item = usize> {
+        let x = i % self.row_len;
+        let y = i / self.row_len;
+
         let x_coords = [
             x.checked_sub(1),
             Some(x),
@@ -68,35 +87,25 @@ impl DayFour {
         let y_coords = [
             y.checked_sub(1),
             Some(y),
-            Some(y + 1).filter(|&c| c < self.col_len()),
+            Some(y + 1).filter(|&c| c < self.col_len),
         ];
 
-        x_coords.into_iter().filter_map(|i| i).flat_map(move |i| {
-            y_coords
-                .into_iter()
-                .filter_map(|j| j)
-                .filter(move |&j| i != x || j != y)
-                .map(move |j| (i, j))
-        })
+        x_coords
+            .into_iter()
+            .filter_map(|i| i)
+            .flat_map(move |i| {
+                y_coords
+                    .into_iter()
+                    .filter_map(|j| j)
+                    .filter(move |&j| i != x || j != y)
+                    .map(move |j| (i, j))
+            })
+            .map(|c| coord_to_index(c, self.row_len))
     }
+}
 
-    fn get(&self, (x, y): Coord) -> bool {
-        self.grid[y * self.col_len() + x]
-    }
-
-    fn get_mut(&mut self, (x, y): Coord) -> &mut bool {
-        let i = y * self.col_len() + x;
-
-        &mut self.grid[i]
-    }
-
-    fn col_len(&self) -> usize {
-        self.grid.len() / self.row_len
-    }
-
-    fn all_coords(&self) -> impl Iterator<Item = Coord> {
-        (0..self.col_len()).flat_map(move |c| (0..self.row_len).map(move |r| (r, c)))
-    }
+fn coord_to_index((x, y): Coord, row_len: usize) -> usize {
+    (y * row_len) + x
 }
 
 #[cfg(test)]
