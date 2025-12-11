73b027654921c31cc58ab04cadabe50d8c7549e9
Sam Rowe <sgrowe@live.co.uk>
2025-12-11 13:35:17 +0000

    Solve day 3 part 2

--- src/day_03.rs
+++ src/day_03.rs
@@ -14,11 +14,11 @@ impl Aoc for DayThree {
     }
 
     fn part_one(&mut self) -> Self::PtOne {
-        self.max_voltages().sum()
+        self.max_voltages::<2>().sum()
     }
 
     fn part_two(&mut self) -> Self::PtTwo {
-        0
+        self.max_voltages::<12>().sum()
     }
 }
 
@@ -27,8 +27,8 @@ impl DayThree {
         self.input.trim().lines().map(|l| parse_digits(l))
     }
 
-    fn max_voltages(&self) -> impl Iterator<Item = usize> {
-        self.battery_banks().map(|b| max_voltage(b))
+    fn max_voltages<const N: usize>(&self) -> impl Iterator<Item = usize> {
+        self.battery_banks().map(|b| max_voltage::<N, _>(b))
     }
 }
 
@@ -36,25 +36,35 @@ fn parse_digits(s: &str) -> impl Iterator<Item = u8> {
     s.chars().map(|c| (c as u8) - 48)
 }
 
-fn max_voltage<B>(mut bank: B) -> usize
+fn max_voltage<const N: usize, B>(mut bank: B) -> usize
 where
     B: Iterator<Item = u8>,
 {
-    let mut a = bank.next().unwrap();
-    let mut b = bank.next().unwrap();
+    let mut digits = [0; N];
+
+    for i in 0..N {
+        digits[i] = bank.next().unwrap();
+    }
 
     for x in bank {
-        if b > a {
-            a = b;
-            b = x;
-        } else if x > b {
-            b = x;
-        } else if x > a {
-            a = x;
+        for i in 1..N {
+            if digits[i] > digits[i - 1] {
+                digits.copy_within(i.., i - 1);
+                digits[N - 1] = x;
+                break;
+            }
+        }
+
+        if x > digits[N - 1] {
+            digits[N - 1] = x;
         }
     }
 
-    ((a * 10) + b) as usize
+    digits
+        .into_iter()
+        .enumerate()
+        .map(|(i, x)| (x as usize) * 10usize.pow((N - i - 1) as u32))
+        .sum()
 }
 
 #[cfg(test)]
@@ -69,9 +79,23 @@ mod tests {
 ";
 
     #[test]
-    fn test_max_voltage() {
-        let banks: Vec<_> = DayThree::new(EXAMPLE.to_string()).max_voltages().collect();
+    fn test_part_one() {
+        let banks: Vec<_> = DayThree::new(EXAMPLE.to_string())
+            .max_voltages::<2>()
+            .collect();
 
-        assert_eq!(banks, vec![98, 89, 78, 92,]);
+        assert_eq!(banks, vec![98, 89, 78, 92]);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let banks: Vec<_> = DayThree::new(EXAMPLE.to_string())
+            .max_voltages::<12>()
+            .collect();
+
+        assert_eq!(
+            banks,
+            vec![987654321111, 811111111119, 434234234278, 888911112111]
+        );
     }
 }
